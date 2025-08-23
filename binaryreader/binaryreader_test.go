e47fd007c56cba26166ea38919d3ca507f25d07f
felipe.bonato <felipe13.bonato@gmail.com>
2025-08-23 11:28:14 -0300

    Add peek test

--- binaryreader/binaryreader_test.go
+++ binaryreader/binaryreader_test.go
@@ -2,12 +2,13 @@ package binaryreader_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/felipe-bonato/goutils/binaryreader"
 )
 
-func TestBinaryReaderByte(t *testing.T) {
+func TestReadByte(t *testing.T) {
 	r := bytes.NewReader([]byte{12, 13, 14})
 
 	br := binaryreader.New(r, binaryreader.LittleEndian)
@@ -28,7 +29,7 @@ func TestBinaryReaderByte(t *testing.T) {
 
 }
 
-func TestBinaryReaderInt(t *testing.T) {
+func TestReadInt32(t *testing.T) {
 	buf := []byte{12, 0, 0, 0, 13, 0, 0, 0, 14, 0, 0, 0}
 	br := binaryreader.New(bytes.NewReader(buf), binaryreader.LittleEndian)
 
@@ -45,5 +46,34 @@ func TestBinaryReaderInt(t *testing.T) {
 	if _, ok := br.Int32(); ok { // Ended, should return false
 		t.Fatal("Should have ended")
 	}
+
+}
+
+func TestPeek(t *testing.T) {
+	buf := []byte{5, 6, 7}
+	r := bytes.NewReader(buf)
+
+	testPeekByte := func(expected byte, expectedOk bool) {
+		b, ok := binaryreader.Peek[byte](r, binaryreader.LittleEndian)
+		if ok != expectedOk {
+			t.Errorf("expectedi ok to be %t, but got %t", expectedOk, ok)
+		}
+		if b != expected {
+			t.Errorf("expected %d, but got %d", expected, b)
+		}
+	}
+
+	testPeekByte(5, true)
+	testPeekByte(5, true)
+
+	// Advance reader
+	r.Seek(1, io.SeekCurrent)
+	testPeekByte(6, true)
+
+	r.Seek(1, io.SeekCurrent)
+	testPeekByte(7, true)
+
+	r.Seek(1, io.SeekCurrent)
+	testPeekByte(0, false)
 
 }
